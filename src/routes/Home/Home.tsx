da548970db0611dda70530903dd8c315b26e0e3d
Logan Thomas <logan.thomas@forsta.com>
2024-03-12 00:32:44 -0600

    changed song

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -20,7 +20,7 @@ function Home() {
   const emailRef = useRef<null | HTMLDivElement>(null);
 
   useEffect(() => {
-    audio.current = new Audio("/back-to-the-matrix.mp3");
+    audio.current = new Audio("/id-4.mp3");
   }, []);
 
   return (
