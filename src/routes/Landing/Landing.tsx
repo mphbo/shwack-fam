70b1f4e7386ecc916d9a8a3f50e71407cc8c48bf
Logan Thomas <logan.thomas@forsta.com>
2024-11-14 03:46:48 -0700

    got rid of comments and added alt to image

--- src/routes/Landing/Landing.tsx
+++ src/routes/Landing/Landing.tsx
@@ -5,15 +5,14 @@ function Landing() {
   return (
     <div className="App">
       <div className={styles.main}>
-        {/* <div>
-          <h1>SHWACK</h1>
-          <h2>ENTERTAINMENT</h2>
-          <h1>FAM</h1>
-        </div> */}
         <Link to="./entertainment" className={styles.button}>
           ENTER
         </Link>
-        <img style={{ width: "100%" }} src={`/landing.svg`} />
+        <img
+          alt="shwack fam logo"
+          style={{ width: "100%" }}
+          src={`/landing.svg`}
+        />
       </div>
     </div>
   );
