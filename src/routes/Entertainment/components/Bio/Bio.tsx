bf9da794a10da75d9e1a54d30e9849896c4daccf
Logan Thomas <logan.thomas@forsta.com>
2024-11-19 12:45:52 -0700

    updated bio

--- src/routes/Entertainment/components/Bio/Bio.tsx
+++ src/routes/Entertainment/components/Bio/Bio.tsx
@@ -7,10 +7,11 @@ function Bio() {
     <div className={styles.bio}>
       <img src="/sfe-small.svg" />
       <span className={styles.bioText}>
-        Shwack Fam biography here - Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Eligendi quam earum iure voluptatum totam, animi
-        officia aperiam! Temporibus facilis porro exercitationem adipisci rem
-        assumenda commodi optio, quisquam consequuntur aliquid ullam.
+        It all starts with music, music brought us together, and music is what
+        we will bring to you. With a large number of djs, music producers and
+        organizers under our banner, S-FAM Entertainment is here to throw you
+        shows that aren't just exciting but diverse as well. Welcome to the
+        show, Welcome to the FAM.
       </span>
       <Socials />
     </div>
